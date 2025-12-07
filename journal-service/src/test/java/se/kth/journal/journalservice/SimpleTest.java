50aed3e3839981f4d0f93780a616c3a7e4dc8d85
Bemnet Tadesse <145583888+Bmt127@users.noreply.github.com>
2025-12-07 14:21:32 +0100

    .

---
+++ journal-service/src/test/java/se/kth/journal/journalservice/SimpleTest.java
@@ -0,0 +1,12 @@
+package se.kth.journal.journalservice;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    void testApplicationLoads() {
+        assertTrue(true);
+    }
+}
