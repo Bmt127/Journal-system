a1d07a8ef7b2562fbd35535be150a6e00392cb45
Bemnet Tadesse <145583888+Bmt127@users.noreply.github.com>
2025-12-27 17:24:15 +0100

    fix

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,3 +1,10 @@
 CREATE DATABASE IF NOT EXISTS journal_users;
 CREATE DATABASE IF NOT EXISTS messagedb;
 CREATE DATABASE IF NOT EXISTS journaldb;
+
+CREATE USER IF NOT EXISTS 'journal'@'%' IDENTIFIED BY 'journal';
+GRANT ALL PRIVILEGES ON journaldb.* TO 'journal'@'%';
+GRANT ALL PRIVILEGES ON journal_users.* TO 'journal'@'%';
+GRANT ALL PRIVILEGES ON messagedb.* TO 'journal'@'%';
+
+FLUSH PRIVILEGES;
