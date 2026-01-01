d42fa95cdb8ec0fa64166ce1336eab5b285fcc4a
Bemnet Tadesse <145583888+Bmt127@users.noreply.github.com>
2026-01-01 16:11:23 +0100

    test 444

--- user-service/src/main/java/se/kth/journal/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/se/kth/journal/userservice/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +24,12 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation(
+                "https://keycloakk.app.cloud.cbh.kth.se/realms/healthcare-realm"
+        );
     }
 }
