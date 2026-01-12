4e02758e1e1ac7dbac84e73f1e84424a3fd775f3
Bemnet Tadesse <145583888+Bmt127@users.noreply.github.com>
2026-01-12 02:01:29 +0100

    fixat

--- frontend/src/api/searchApi.js
+++ frontend/src/api/searchApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { attachAuthInterceptor } from "./axiosConfig";
 export const searchApi = axios.create({
-    // Om namnet i dashboarden är search-service, ta bort S:et:
-    baseURL: "https://search-service.app.cloud.cbh.kth.se"
+    // Denna MÅSTE ha ett s på slutet om dashboarden säger search-services
+    baseURL: "https://search-services.app.cloud.cbh.kth.se"
 });
 attachAuthInterceptor(searchApi);
